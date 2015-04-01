66c806898bb95225219573f8a09c68a8dc5e7ad9
scottchen625 <scottchen625@gmail.com>
2015-03-31 21:51:00 -0700

    add empty ProjectEulerID_04 for git testing

---
+++ ProjectEulerID_04/ProjectEulerID_04/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectEulerID_04
+//
+//  Created by Scott Chen on 3/31/15.
+//  Copyright (c) 2015 Chen, Scott. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
